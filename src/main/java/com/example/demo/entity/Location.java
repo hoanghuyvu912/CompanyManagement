0a74cdfd97a05113ea7ae2302e9123abb9a49ef9
Hoang Vu Huy <vhhoang@axonactive.com>
2023-05-15 17:53:52 +0700

    Add unit test for findAllDepartment

--- src/main/java/com/example/demo/entity/Location.java
+++ src/main/java/com/example/demo/entity/Location.java
@@ -1,6 +1,7 @@
 package com.example.demo.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import javax.persistence.*;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Table(name="department_location")
 public class Location {
     @Id
